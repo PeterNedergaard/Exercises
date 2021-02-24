3323d43e6146dba02f0b914e4341bfe7e8f0bdd8
Peter Nedergaard <Peter@nedergaard.org>
2021-02-24 13:13:26 +0100

    tasks

---
+++ week8/taskOne/Main.java
@@ -0,0 +1,20 @@
+class Main{
+	public static void main(String[] args){
+		emptyLine();
+		printLine("This is a line");
+		printNameAge("Peter",19);
+
+	}
+
+	public static void emptyLine(){
+		System.out.println("");
+	}
+
+	public static void printLine(String line){
+		System.out.println(line);
+	}
+
+	public static void printNameAge(String name, int age){
+		System.out.println("My name is " + name + ", I am " + age + " years old");
+	}
+}
