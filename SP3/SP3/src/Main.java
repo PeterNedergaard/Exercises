1065ddd3b5030dda072df4e66d92d80881e4c21e
Peter Nedergaard <78910318+PeterNedergaard@users.noreply.github.com>
2021-04-15 15:51:59 +0200

    moved SP3

--- SP3/SP3/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        //Tournament tournament = new Tournament();
-        Ui.startOrContinueGame();
-        Ui ui = new Ui();
-    }
-}
